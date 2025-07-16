b9e3cea3a8f55d382049980eab4adf49b7d44b96
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2025-07-16 12:41:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,35 +24,34 @@ def suggestSimilar(current):
   return list(reversed(toSuggest))
 
 def test():
-  global iterations, posts, times, accuracy
-  for x in range(iterations):
-    for i in range(len(posts)):
-      start_time = time.time()
-      similar = suggestSimilar(posts[i])
-      times.append(time.time() - start_time)
-      accuracy.append(similar[0][1])
-      print(f"Post {i} (iter {x})'s best match: ")
-      print(f"Body text of original: {posts[i][0]}")
-      print(f"Body text of match: {similar[0][0][0]}")
-      print(f"Tags of match: {similar[0][0][1]}")
-      print(f"Tags of original: {posts[i][1]}")
-      print(f"Percent match: {similar[0][1]}%")
-      print("\n")
-      time.sleep(0.1)
+  global iterations, posts, times, accuracy, y
+  for k in range(len(posts)):
+    start_time = time.time()
+    similar = suggestSimilar(posts[k])
+    times.append(time.time() - start_time)
+    accuracy.append(similar[0][1])
+    print(f"Post {k+1} (iter {y+1})'s best match: ")
+    print(f"Body text of original: {posts[k][0]}")
+    print(f"Body text of match: {similar[0][0][0]}")
+    print(f"Tags of match: {similar[0][0][1]}")
+    print(f"Tags of original: {posts[k][1]}")
+    print(f"Percent match: {similar[0][1]}%")
+    print("\n")
+    time.sleep(0.1)
 
-posts = []
-postNumber = 200
+postNumber = 100
 iterations = 2
 
-for i in range(postNumber):
-  tags = []
-  for x in range(round(math.log(postNumber))):
-    tags.append(f"Example Tag {random.randint(1, round(math.log(postNumber))*3)}")
-  addPost(f"{adj[random.randint(0, len(adj)-1)] + ' ' + nouns[random.randint(0, len(nouns)-1)] + ' ' + verbs[random.randint(0, len(verbs)-1)] + ' ' + adv[random.randint(0, len(adv)-1)] + punct[random.randint(0, len(punct)-1)]}",tags)
-
-times = [] # Speed test, caclulate average performance and accuracy, for posts of diffrent text
+times = [] # Caclulate average performance and accuracy, for posts of diffrent text
 accuracy = []
-test()
+for y in range(iterations):
+  posts = []
+  for i in range(postNumber):
+    tags = []
+    for x in range(round(math.log(postNumber))):
+      tags.append(f"Example Tag {random.randint(1, round(math.log(postNumber))*3)}")
+    addPost(f"{adj[random.randint(0, len(adj)-1)] + ' ' + nouns[random.randint(0, len(nouns)-1)] + ' ' + verbs[random.randint(0, len(verbs)-1)] + ' ' + adv[random.randint(0, len(adv)-1)] + punct[random.randint(0, len(punct)-1)]}",tags)
+  test()
 
 print(f"{NateUtils.average(times)}sec per match completed")
 print(f"{NateUtils.average(accuracy)}% average accuracy")
@@ -60,13 +59,16 @@ print(f"{max(accuracy)}% most accurate match")
 
 input("Enter to Continue")
 
-posts = []
-for i in range(postNumber):
-  addPost(f"Example",[f"Example Tag {random.randint(1, round(postNumber/10))}",f"Example Tag {random.randint(1, round(postNumber/10))}",f"Example Tag {random.randint(1, round(postNumber/10))}",f"Example Tag {random.randint(1, round(postNumber/10))}",f"Example Tag {random.randint(1, round(postNumber/10))}"])
-
-times = [] # Speed test, caclulate average performance and accuracy for posts of the same text
+times = [] # Caclulate average performance and accuracy for posts of the same text
 accuracy = []
-test()
+for y in range(iterations):
+  posts = []
+  for i in range(postNumber):
+    tags = []
+    for x in range(round(math.log(postNumber))):
+      tags.append(f"Example Tag {random.randint(1, round(math.log(postNumber))*3)}")
+    addPost(f"Example",tags)
+  test()
 
 print(f"{NateUtils.average(times)}sec per match completed")
 print(f"{NateUtils.average(accuracy)}% average accuracy")
